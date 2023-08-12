8723cc5c4841335b81a37b7228fa7c2f9bf66d00
Juan Mignaco <goleadornato@yahoo.com>
2023-08-12 11:46:16 -0400

    Add API to docker and update docker composer

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const PORT: string | number = process.env.PORT || 4000
 app.use(cors());
 app.use(todoRoutes);
 
-const uri: string = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@localhost:27017/${process.env.MONGO_DB}?authSource=admin`;
+const uri: string = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.DB_HOST}:27017/${process.env.MONGO_DB}?authSource=admin`;
 
 console.log(`uri: ${uri}`);
 
